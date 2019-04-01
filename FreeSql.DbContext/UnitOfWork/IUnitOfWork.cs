b48a6d224a9f38acea1cecaf61c8e5f9e3e078c8
28810 <28810@YEXIANGQIN>
2019-04-01 10:47:49 +0800

    v0.4.1 init

---
+++ FreeSql.DbContext/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace FreeSql {
+	public interface IUnitOfWork : IDisposable {
+
+		DbTransaction GetOrBeginTransaction(bool isCreate = true);
+
+		void Commit();
+
+		void Rollback();
+	}
+}

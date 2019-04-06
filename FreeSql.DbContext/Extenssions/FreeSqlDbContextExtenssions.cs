4d97669334bf32c88a8b03dbdba4854551c8a1dc
2881099 <2881099@qq.com>
2019-04-06 11:23:39 +0800

    增加扩展方法 NoTracking，不跟踪数据

--- FreeSql.DbContext/Extenssions/FreeSqlDbContextExtenssions.cs
+++ FreeSql.DbContext/Extenssions/FreeSqlDbContextExtenssions.cs
@@ -10,4 +10,14 @@ public static class FreeSqlDbContextExtenssions {
 	public static DbContext CreateDbContext(this IFreeSql that) {
 		return new FreeContext(that);
 	}
+
+	/// <summary>
+	/// 不跟踪查询的实体数据（在不需要更新其数据时使用），可提长查询性能
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="select"></param>
+	/// <returns></returns>
+	public static ISelect<T> NoTracking<T>(this ISelect<T> select) where T : class {
+		return select.TrackToList(null);
+	}
 }
